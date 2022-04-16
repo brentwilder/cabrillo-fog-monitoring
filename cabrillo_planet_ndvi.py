a9f647a45f8afcf6729fabce0ae055ffb10623d2
brentwilder <brentonwilder1995@gmail.com>
2022-04-16 12:44:20 -0600

    ndvi as function

--- cabrillo_planet_ndvi.py
+++ cabrillo_planet_ndvi.py
@@ -1,47 +1,48 @@
 import rasterio
 import numpy
 from xml.dom import minidom
-# TO-DO: Assign img variables that can be repeatable easier
 
-image_file = "./data/20210711_183242/20210711_183242_20_240c_3B_AnalyticMS_SR_clip.tif"
+# Inputs for function.. to be scaled up in future
+image = "./data/20210711_183242/20210711_183242_20_240c_3B_AnalyticMS_SR_clip.tif"
+xml = "./data/20210711_183242/20210711_183242_20_240c_3B_AnalyticMS_metadata_clip.xml"
+output = './data/20210711_183242/ndvi.tif'
 
-# Load red and NIR bands - note all PlanetScope 4-band images have band order BGRN
-with rasterio.open(image_file) as src:
-    band_red = src.read(3)
+def create_ndvi_raster(image,xml,output):
+    # Load red and NIR bands - note all PlanetScope 4-band images have band order BGRN
+    with rasterio.open(image) as src:
+        band_red = src.read(3)
 
-with rasterio.open(image_file) as src:
-    band_nir = src.read(4)
+    with rasterio.open(image) as src:
+        band_nir = src.read(4)
 
-xmldoc = minidom.parse("./data/20210711_183242/20210711_183242_20_240c_3B_AnalyticMS_metadata_clip.xml")
-nodes = xmldoc.getElementsByTagName("ps:bandSpecificMetadata")
+    xmldoc = minidom.parse(xml)
+    nodes = xmldoc.getElementsByTagName("ps:bandSpecificMetadata")
 
-# XML parser refers to bands by numbers 1-4
-coeffs = {}
-for node in nodes:
-    bn = node.getElementsByTagName("ps:bandNumber")[0].firstChild.data
-    if bn in ['1', '2', '3', '4']:
-        i = int(bn)
-        value = node.getElementsByTagName("ps:reflectanceCoefficient")[0].firstChild.data
-        coeffs[i] = float(value)
-        
-        
-        
-# Multiply by corresponding coefficients
-band_red = band_red * coeffs[3]
-band_nir = band_nir * coeffs[4]
+    # XML parser refers to bands by numbers 1-4
+    coeffs = {}
+    for node in nodes:
+        bn = node.getElementsByTagName("ps:bandNumber")[0].firstChild.data
+        if bn in ['1', '2', '3', '4']:
+            i = int(bn)
+            value = node.getElementsByTagName("ps:reflectanceCoefficient")[0].firstChild.data
+            coeffs[i] = float(value)
+            
+    # Multiply by corresponding coefficients
+    band_red = band_red * coeffs[3]
+    band_nir = band_nir * coeffs[4]
 
-# Allow division by zero
-numpy.seterr(divide='ignore', invalid='ignore')
+    # Allow division by zero
+    numpy.seterr(divide='ignore', invalid='ignore')
 
-# Calculate NDVI
-ndvi = (band_nir.astype(float) - band_red.astype(float)) / (band_nir + band_red)
+    # Calculate NDVI
+    ndvi = (band_nir.astype(float) - band_red.astype(float)) / (band_nir + band_red)
 
-# Set spatial characteristics of the output object to mirror the input
-kwargs = src.meta
-kwargs.update(
-    dtype=rasterio.float32,
-    count = 1)
+    # Set spatial characteristics of the output object to mirror the input
+    kwargs = src.meta
+    kwargs.update(
+        dtype=rasterio.float32,
+        count = 1)
 
-# Create the file
-with rasterio.open('./data/20210711_183242/ndvi.tif', 'w', **kwargs) as dst:
-        dst.write_band(1, ndvi.astype(rasterio.float32))
+    # Create the file
+    with rasterio.open(output, 'w', **kwargs) as dst:
+            dst.write_band(1, ndvi.astype(rasterio.float32))
