52c0e3651d4be38dbd26dcc98354af7734646478
Brent Wilder <56372982+brentwilder@users.noreply.github.com>
2022-04-28 19:21:28 -0600

    code ready for testing at the park

--- cabrillo_fog_detect.R
+++ cabrillo_fog_detect.R
@@ -6,35 +6,57 @@
 library(hazer)
 library(jpeg)
 
-# Set input for function
-# Depending on file structure... it could  be something like,
-# taking in all recent files loopning through in a function on a schedule (i.e., taskscheduleR)
-# Then, output the spatial/temporal information to a running csv database
-# Finally, should create a working UI that activley reads the csv and outputs to a cool web GIS platform
-image <- "test.jpg"
+# Set input (camera) for function
+# This needs to be a folder of .jpgs in your working directory
+camera <- 'test_camera1'
 
 # Function that takes in a jpg an outputs haze degree and classification
 # User can also define the threshold for classifying fog and no-fog
-fog_function <- function(image,threshold=0.4) {
+fog_function <- function(camera,threshold=0.4) {
 
-  # read the image as an array
-  rgb_array <- jpeg::readJPEG(image)
+  # perform concatenation
+  folder <- paste('./', camera, sep ='')
+  
+  # call all files that are jpg
+  files <- list.files(path=folder, pattern='.jpg')
+  
+  # create data frame with 0 rows and 3 columns
+  df <- data.frame(matrix(ncol = 3, nrow = 0))
 
-  # extracting the haze matrix
-  haze_matrix <- getHazeFactor(rgb_array)
-  haze_degree <- haze_matrix[c('haze')] 
-  if(haze_degree > threshold){
-    fog <- 1
-  } else {
-    fog <- 0
+  # provide column names
+  colnames(df) <- c('timestamp', 'haze_degree', 'fog')
+  
+  # start loop for all jpgs
+  for (i in files){
+    
+    # perform concatenation
+    image <- paste('./', camera,'/', i , sep ='')
+    
+    # read the image as an array
+    rgb_array <- jpeg::readJPEG(image)
+
+    # extracting the haze matrix
+    haze_matrix <- getHazeFactor(rgb_array)
+    haze_degree <- haze_matrix[c('haze')] 
+    if(haze_degree > threshold){
+      fog <- 1
+    } else {
+      fog <- 0
+    }
+
+    # remove jpg from the name
+    i <- substr(i,1,nchar(i)-4)
+
+    # append the new image data to a new row
+    df[nrow(df) + 1,] = c(i, haze_degree, fog)
   }
+  
+  # perform concatenation for output
+  output <- paste('./output/rhazer_', camera,'.csv', sep ='')
 
-  return(list(haze_degree,fog))
-
+  # save the data to a csv file
+  write.csv(df,output, row.names = FALSE)
+  return()
 }
 
-print(fog_function(image))
-
-
-
-
+fog_function(camera)
